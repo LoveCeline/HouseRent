e1a7a2bfb83941d6e10b5877b706266d29e17af8
LoveCeline <wuxiaoli410@tom.com>
2014-04-23 15:00:16 +0800

    div布局div布局

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@
  */
 
 exports.index = function(req, res){
+    res.render('frontViews/Index', { title: '租房管理' });
+};
+exports.hindIndex = function(req, res){
     res.render('hindViews/hindIndex', { title: '租房管理' });
 };
