494242963fed1abe91fa68f41a58bee8ae6090b6
LoveCeline <wuxiaoli410@tom.com>
2014-04-23 16:11:03 +0800

    div布局div布局

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
--- public/controller/app.js
+++ public/controller/app.js
@@ -1,3 +1,4 @@
 /**
  * Created by piero on 14-4-9.
  */
+angular.module('HouseRent', []);
